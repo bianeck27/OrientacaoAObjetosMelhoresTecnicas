a91dd3f08cfb9ff603496cfdbcdcd754bbb82321
Thiago Bianeck <bianeck27@gmail.com>
2018-02-20 19:45:34 -0300

    Arquivos finais

---
+++ FJ-93/src/ArquivoDeDividas.java
@@ -0,0 +1,20 @@
+
+public class ArquivoDeDividas implements ArmazenadorDeDividas{
+
+	private File Arquivo;
+	
+	public
+	
+	@Override
+	public void salva(Divida divida) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Divida carrega(Documento documentoCredor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
